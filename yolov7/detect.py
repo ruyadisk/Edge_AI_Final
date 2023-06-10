93e752608076f45fd932ad85c6b99c6727badda4
ruyadisk <84979672+ruyadisk@users.noreply.github.com>
2023-06-10 14:25:37 +0800

    Update detect.py

--- yolov7/detect.py
+++ yolov7/detect.py
@@ -171,12 +171,12 @@ def detect(save_img=False):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', nargs='+', type=str, default='./best.pt', help='model.pt path(s)')
-    parser.add_argument('--source', type=str, default='C:\\Users\\cjweibb\\Downloads\\video.mp4', help='source')  # file/folder, 0 for webcam
+    parser.add_argument('--source', type=str, default='../inference/video.mp4', help='source')  # file/folder, 0 for webcam
     parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.55, help='object confidence threshold')
     parser.add_argument('--iou-thres', type=float, default=0.1, help='IOU threshold for NMS')
     parser.add_argument('--device', default='0', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--view-img', action='store_true', help='display results')
+    parser.add_argument('--view-img', defaulte = True, action='store_true', help='display results')
     parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
     parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
     parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
@@ -187,14 +187,14 @@ if __name__ == '__main__':
     parser.add_argument('--project', default='runs/detect', help='save results to project/name')
     parser.add_argument('--name', default='exp', help='save results to project/name')
     parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
-    parser.add_argument('--no-trace', action='store_true', help='don`t trace model')
+    parser.add_argument('--no-trace', default = True, action='store_true', help='don`t trace model')
     opt = parser.parse_args()
     print(opt)
     #check_requirements(exclude=('pycocotools', 'thop'))
 
     with torch.no_grad():
         if opt.update:  # update all models (to fix SourceChangeWarning)
-            for opt.weights in ['C:\\Users\\cjweibb\\anaconda3\\envs\\edge_ai_yolov7\\yolov7\\runs\\train\\yolov7-custom\\weights\\best.pt']:
+            for opt.weights in ['./best.pt']:
                 detect()
                 strip_optimizer(opt.weights)
         else:
